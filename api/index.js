f6ed41cd151bec02227974044278d393b3453d51
deepak199395 <Deepak19939426@gmail.com>
2026-04-22 11:48:23 +0530

    adding .env file

--- api/index.js
+++ api/index.js
@@ -1,3 +1,13 @@
+const connectDb = require("../Config/Db");
 const app = require("../app");
 
-module.exports = app;
+let isConnected = false;
+
+module.exports = async (req, res) => {
+  if (!isConnected) {
+    await connectDb();
+    isConnected = true;
+    console.log("DB connected on Vercel");
+  }
+  return app(req, res);
+};
