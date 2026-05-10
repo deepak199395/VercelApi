4886c82548b2dd857240d7ea4e147262c103f2b4
deepak199395 <Deepak19939426@gmail.com>
2026-05-10 17:35:09 +0530

    add localhost 3001

--- app.js
+++ app.js
@@ -26,6 +26,8 @@ const allowedOrigins = [
   "https://www.shrigaar.com",
   "https://shrigaar-dashboard.web.app",
   "http://localhost:3000",
+  "http://localhost:3001",
+  "http://localhost:3002",
   "http://localhost:5001",
   "https://shringaars.com"
 ];
